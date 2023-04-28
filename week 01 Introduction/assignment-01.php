70e3327ae3c48d64a1bf760e24855e0a7bbf1742
Eman Ghazy <75549105+Eman1-2-3@users.noreply.github.com>
2023-04-28 21:20:04 +0200

    Add files via upload

---
+++ week 01 Introduction/assignment-01.php
@@ -0,0 +1,20 @@
+<?php
+/*
+i
+love
+php
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?='My First PHP Page'?></title>
+</head>
+<body>
+<div><?php echo 'We Love'?></div>
+<div><?php echo 'Elzero Channel'?></div>
+</body>
+</html>
