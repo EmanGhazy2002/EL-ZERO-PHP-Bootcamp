b8fab15c86dc0ddbb91a4682694e9da9e13265d5
Eman Ghazy <75549105+Eman1-2-3@users.noreply.github.com>
2023-04-29 23:58:17 +0200

    Update assignment-08.php

--- week 02 Data Types[6-12]/assignment-08.php
+++ week 02 Data Types[6-12]/assignment-08.php
@@ -9,3 +9,41 @@ print_r(["FrontEnd"=>
    "Testing"=>["Unit Testing","End To End","Integration"]
     ]);
 echo "</pre>";
+
+// Needed Output
+/*
+Array
+(
+  [FrontEnd] => Array
+    (
+      [0] => HTML
+      [1] => CSS
+      [JS] => Array
+        (
+          [Vuejs] => Array
+            (
+              [2] => v2
+              [3] => v3
+            )
+
+          [0] => Reactjs
+          [1] => Svelte
+        )
+    )
+
+  [BackEnd] => Array
+    (
+      [0] => PHP
+      [1] => MySQL
+      [2] => Security
+    )
+
+  [0] => Git
+  [1] => Github
+  [Testing] => Array
+    (
+      [0] => Unit Testing
+      [1] => End To End
+      [2] => Integration
+    )
+)  */
