e4151f55056bb6bf56617061fa6dd27be74f1839
Eman Ghazy <75549105+Eman1-2-3@users.noreply.github.com>
2023-04-29 23:46:11 +0200

    Update assignment-04.php

--- week 01 Introduction[1-5]/assignment-04.php
+++ week 01 Introduction[1-5]/assignment-04.php
@@ -1,5 +1,5 @@
 <?php
 // ## First Comment         #(this comment is true)
 # // # Second Comment       #(this comment is true)
-/* /* /* Third Comment */   #(this comment is true)
+/* /* /* Third Comment */   #(this comment is false)
 ////// Fourth Comment       #(this comment is true)
